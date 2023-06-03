fa7964db39f5bff0586038541930ccf96542ef61
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-04 01:07:27 +0200

    Fix duplication when scrolling

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,58 +6,41 @@ import { faJedi } from '@fortawesome/free-solid-svg-icons';
 import { StarshipData } from '../../components/StarshipData/StarshipData';
 
 export const StarshipList = () => {
+  const element = <FontAwesomeIcon icon={faJedi} />;
+  const [page, setPage] = useState(1);
+  const [starship, setStarship] = useState([]);
 
-    const element = <FontAwesomeIcon icon={faJedi} />
-    const [page, setPage] = useState(1);
-    const [viewMore, setViewMore] = useState(true);
-    const [starship, setStarship] = useState([]);
+  const fetchSpaceships = () => {
+    axios.get(`https://swapi.dev/api/starships/?page=${page}`).then((res) => {
+      setStarship((prevStarship) => prevStarship.concat(res.data.results));
+    });
+  };
 
-    const fetchSpaceships = () => {
-        axios
-        .get(`https://swapi.dev/api/starships/?page=${page}`)
-        .then((res) => {
-            setStarship(prevStarship => prevStarship.concat(res.data.results))
-        })}
+  useEffect(() => {
+    fetchSpaceships();
+  }, [page]);
 
-        const scrollLoading = () => {
-            axios
-            .get(`https://swapi.dev/api/starships/?page=${page}`)
-            .then((res) => {
-                res.data.next == null ? setViewMore(false) : setViewMore(true);
-            })
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={starship.length}
+        next={() => setPage((page) => (page < 4 ? page + 1 : page))}
+        loader={<h1>Loading...</h1>}
+        endMessage={
+          <p style={{ color: 'gold', fontSize: '1.3rem', textAlign: 'center' }}>
+            All starships successfully loaded!
+            <br />
+            <br />
+            {element}
+          </p>
         }
-        const savedValue = localStorage.getItem('logged');
-        const newValue = JSON.parse(savedValue);
-
-        useEffect(() => {
-            fetchSpaceships()
-            scrollLoading()
-        }, [page]);
-
-        return (
-            <div>
-                {
-                    <InfiniteScroll
-                    dataLength={starship.length}
-                    next={() => setPage(page => page < 4 ? page + 1 : page)}
-                    hasMore={viewMore} // Update the prop name from `viewMore` to `hasMore`
-                    loader={<h1>Loading...</h1>}
-                    endMessage={
-                      <p style={{ color: 'gold', fontSize: '1.3rem', textAlign: 'center' }}>
-                        All starships successfully loaded!
-                        <br />
-                        <br />
-                        {element}
-                      </p>
-                    }
-                  >
-                    <div className='starshipCard'>
-                      {starship.map((ship) => (
-                        <StarshipData key={ship.url} name={ship.name} model={ship.model} url={ship.url} />
-                      ))}
-                    </div>
-                  </ InfiniteScroll>
-                }
-            </div>
-        )
-    };
+      >
+        <div className='starshipCard'>
+          {starship.map((ship) => (
+            <StarshipData key={ship.url} name={ship.name} model={ship.model} url={ship.url} />
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
