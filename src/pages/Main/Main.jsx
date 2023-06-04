f9c8e0b810144e851876e019c78f902efe8e1a2c
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-04 02:09:48 +0200

    Fix duplicates, but there's a fetching delay

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,58 +6,55 @@ import { faJedi } from '@fortawesome/free-solid-svg-icons';
 import { StarshipData } from '../../components/StarshipData/StarshipData';
 
 export const StarshipList = () => {
+  const element = <FontAwesomeIcon icon={faJedi} />;
+  const [page, setPage] = useState(1);
+  const [viewMore, setViewMore] = useState(true);
+  const [starship, setStarship] = useState([]);
 
-    const element = <FontAwesomeIcon icon={faJedi} />
-    const [page, setPage] = useState(1);
-    const [viewMore, setViewMore] = useState(true);
-    const [starship, setStarship] = useState([]);
+  const fetchSpaceships = () => {
+    axios.get(`https://swapi.dev/api/starships/?page=${page}`).then((res) => {
+      setStarship((prevStarship) => {
+        const newStarships = res.data.results.filter((ship) => {
+          return !prevStarship.some((prevShip) => prevShip.url === ship.url);
+        });
+        return prevStarship.concat(newStarships);
+      });
+    });
+  };
 
-    const fetchSpaceships = () => {
-        axios
-        .get(`https://swapi.dev/api/starships/?page=${page}`)
-        .then((res) => {
-            setStarship(prevStarship => prevStarship.concat(res.data.results))
-        })}
+  const scrollLoading = () => {
+    axios.get(`https://swapi.dev/api/starships/?page=${page}`).then((res) => {
+      setViewMore(res.data.next !== null);
+    });
+  };
 
-        const scrollLoading = () => {
-            axios
-            .get(`https://swapi.dev/api/starships/?page=${page}`)
-            .then((res) => {
-                res.data.next == null ? setViewMore(false) : setViewMore(true);
-            })
+  useEffect(() => {
+    fetchSpaceships();
+    scrollLoading();
+  }, [page]);
+
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={starship.length}
+        next={() => setPage((prevPage) => prevPage < 4 ? prevPage + 1 : prevPage)}
+        hasMore={viewMore}
+        loader={<h1 style={{ position: 'relative', marginTop: '10rem', color: 'gold', fontSize: '2rem', fontWeight: '100', textAlign: 'center' }}>Loading...</h1>}
+        endMessage={
+          <p style={{ color: 'gold', fontSize: '1.3rem', textAlign: 'center' }}>
+            All starships successfully loaded!
+            <br />
+            <br />
+            {element}
+          </p>
         }
-        const savedValue = localStorage.getItem('logged');
-        const newValue = JSON.parse(savedValue);
-
-        useEffect(() => {
-            fetchSpaceships()
-            scrollLoading()
-        }, [page]);
-
-        return (
-            <div>
-                {
-                    <InfiniteScroll
-                    dataLength={starship.length}
-                    next={() => setPage(page => page < 4 ? page + 1 : page)}
-                    hasMore={viewMore}
-                    loader={<h1>Loading...</h1>}
-                    endMessage={
-                      <p style={{ color: 'gold', fontSize: '1.3rem', textAlign: 'center' }}>
-                        All starships successfully loaded!
-                        <br />
-                        <br />
-                        {element}
-                      </p>
-                    }
-                  >
-                    <div className='starshipCard'>
-                      {starship.map((ship) => (
-                        <StarshipData key={ship.url} name={ship.name} model={ship.model} url={ship.url} />
-                      ))}
-                    </div>
-                  </ InfiniteScroll>
-                }
-            </div>
-        )
-    };
+      >
+        <div className='starshipCard'>
+          {starship.map((ship, index) => (
+            <StarshipData key={`${ship.url}-${index}`} name={ship.name} model={ship.model} url={ship.url} />
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
