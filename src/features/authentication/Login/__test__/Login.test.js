cc3d5486541cee3c397173a2937ceb49a406820a
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-05 23:56:40 +0200

    Improve Login.test.js

--- src/features/authentication/Login/__test__/Login.test.js
+++ src/features/authentication/Login/__test__/Login.test.js
@@ -1,14 +1,35 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { Login } from '../Login';
 
-test('updating form inputs should update the state', () => {
-  const { getByPlaceholderText } = render(<Login />);
-  const emailInput = screen.getByPlaceholderText('Email Address');
-  const passwordInput = screen.getByPlaceholderText('Password');
+describe('Login', () => {
+  it('should display an alert for incorrect credentials', async () => {
+    const closeModalMock = jest.fn();
+    const handleLogInMock = jest.fn();
+    const openRegistrationModalMock = jest.fn();
 
-  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-  fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    const { getByPlaceholderText, getByText, queryByText } = render(
+      <MemoryRouter>
+        <Login
+          closeModal={closeModalMock}
+          handleLogIn={handleLogInMock}
+          openRegistrationModal={openRegistrationModalMock}
+        />
+      </MemoryRouter>
+    );
 
-  expect(emailInput.value).toBe('test@example.com');
-  expect(passwordInput.value).toBe('password123');
+    // Fills in the form input with incorrect credentials.
+    fireEvent.change(getByPlaceholderText('Email Address'), { target: { value: 'invalid@example.com' } });
+    fireEvent.change(getByPlaceholderText('Password'), { target: { value: 'wrongpassword' } });
+    //Submits the form.
+    fireEvent.submit(getByPlaceholderText('Email Address'));
+
+    await waitFor(() => {
+      expect(queryByText('Incorrect credentials. Please, try again.')).toBeInTheDocument();
+    });
+
+    expect(handleLogInMock).not.toHaveBeenCalled();
+    expect(closeModalMock).not.toHaveBeenCalled();
+  });
 });
